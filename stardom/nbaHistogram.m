5eda55c2dea4ce341855a2ad1faa8c873b2c1402
Tyler Lee <tlee753@gmail.com>
2017-07-08 18:42:01 -0400

    clean up

--- stardom/nbaHistogram.m
+++ stardom/nbaHistogram.m
@@ -4,7 +4,8 @@ points = data(1:271, 2);
 rebounds = data(1:271, 15);
 assists = data(1:271, 16);
 
-score = points + rebounds * 2 + assists * 2.333;
+score = points + rebounds * 1.78 + assists * 2.333;
+taken = data(1:271, 5);
 
 close all;
 figure;
@@ -12,4 +13,11 @@ histogram(score);
 title('NBA Proprietary Score Histogram');
 xlabel('Proprietary Score');
 ylabel('Frequency');
+
+figure;
+plot(taken, score, 'ko');
+title('Proprietary Score vs Shots Taken');
+xlabel('Shots Taken');
+ylabel('Proprietary Score');
+
 end
